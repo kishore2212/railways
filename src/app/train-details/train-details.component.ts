efbcc67369964f810e14b177577eaa4df321ed63
Kishore R <kishore.ramesh@zohocorp.com>
2019-11-30 07:38:51 +0530

    minor updates

--- src/app/train-details/train-details.component.ts
+++ src/app/train-details/train-details.component.ts
@@ -12,7 +12,10 @@ export class TrainDetailsComponent implements OnInit {
   trainDetails=[
     {source:"Salem", destination:"Chennai" , no:"12345", name:"Central Express" , start:"12:30", end:"17:15",sl:"120",threeA:"100",twoA:"60"},
     {source:"Salem", destination:"Coimbatore" , no:"12323", name:"Coimbatore Express" , start:"15:30", end:"17:15",sl:"120",threeA:"100",twoA:"60"},
-    {source:"Salem", destination:"Banglore" , no:"32123", name:"Banglore Express" , start:"8:30", end:"11:40",sl:"120",threeA:"100",twoA:"60"}
+    {source:"Salem", destination:"Banglore" , no:"32123", name:"Banglore Express" , start:"8:30", end:"11:40",sl:"120",threeA:"100",twoA:"60"},
+    {source:"Salem", destination:"Chennai" , no:"98765", name:"SA-MAS Express" , start:"8:30", end:"11:40",sl:"120",threeA:"100",twoA:"60"},
+    {source:"Salem", destination:"Coimbatore" , no:"76584", name:"SA-CBE Express" , start:"8:30", end:"11:40",sl:"120",threeA:"100",twoA:"60"}
+
   ];
   buttonAction(selectedTrain){
     console.log(selectedTrain);
